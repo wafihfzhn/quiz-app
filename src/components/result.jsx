a93ca3fc29d91a2e5215960f9dd8f9c547e771b6
wafihfzhn <hafizhuddin96@gmail.com>
2025-05-21 14:32:10 +0800

    Remove redundant

--- src/components/result.jsx
+++ src/components/result.jsx
@@ -11,12 +11,10 @@ function Result(props) {
     return finalScore;
   }
 
-  const userScore = getScore();
-
   return (
     <div>
       <h2>Quiz Completed!</h2>
-      <p>Your Score: {userScore}/{props.totalQuestions}</p>
+      <p>Your Score: {getScore()}/{props.totalQuestions}</p>
 
       <button className="restart-button" onClick={props.restartQuiz}>Restart Quiz</button>
     </div>
