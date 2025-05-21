5dd8bed970439eb2b6dace3eb92c013360ecc4b2
wafihfzhn <hafizhuddin96@gmail.com>
2025-05-21 14:24:54 +0800

    Completed the user result

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -1,62 +1,56 @@
 import { useState } from "react";
+import { questionBanks } from '../data/questionBanks';
 import Result from "./result";
 
 function Quiz() {
-  const questionBanks = [
-    {
-      question: "What is the capital of France?",
-      options: [ "Berlin", "London", "Paris", "Rome" ],
-      answer: "Paris",
-    },
-    {
-      question: "Which language is used for web apps",
-      options: [ "Ruby", "Python", "Javascript", "All"  ],
-      answer: "Paris",
-    },
-    {
-      question: "What does JSX stand for?",
-      options: [
-        "Javascript XML",
-        "Java Syntax Extension",
-        "Just a Simple Example",
-        "None of the above",
-      ],
-      answer: "Javascript XML",
-    },
-  ]
+  const firstIndexQuestion = 0;
+  const initialAnswers = [ null, null, null ];
+  const totalQuestions = questionBanks.length;
 
-  const [ userAnswers, setUserAnswers ] = useState([ null, null, null ]);
-  const [ currentNumQuestion, setCurrentNumQuestion ] = useState(0);
+  const [ userAnswers, setUserAnswers ] = useState(initialAnswers);
+  const [ currentIndexQuestion, setCurrentIndexQuestion ] = useState(firstIndexQuestion);
 
-  const currentQuestion = questionBanks[currentNumQuestion];
-  const isQuizFinished = currentNumQuestion === questionBanks.length
+  const currentQuestion = questionBanks[currentIndexQuestion];
+  const isQuizFinished = currentIndexQuestion === totalQuestions
 
   function handleSelectOption(option) {
     const newUserAnswers = [ ...userAnswers ];
-    newUserAnswers[currentNumQuestion] = option;
+    newUserAnswers[currentIndexQuestion] = option;
 
     setUserAnswers(newUserAnswers);
   }
 
   function prevQuestion() {
-    setCurrentNumQuestion(currentNumQuestion - 1)
+    setCurrentIndexQuestion(currentIndexQuestion - 1);
   }
 
   function nextQuestion() {
-    setCurrentNumQuestion(currentNumQuestion + 1)
+    setCurrentIndexQuestion(currentIndexQuestion + 1);
+  }
+
+  function restartQuiz() {
+    setCurrentIndexQuestion(firstIndexQuestion);
+    setUserAnswers(initialAnswers);
   }
 
   if (isQuizFinished ) {
-    return <Result />
+    return (
+      <Result
+        totalQuestions={totalQuestions}
+        questionBanks={questionBanks}
+        userAnswers={userAnswers}
+        restartQuiz={restartQuiz}
+      />
+    )
   } else {
     return (
       <div>
-        <h3>Question {currentNumQuestion + 1}</h3>
+        <h3>Question {currentIndexQuestion + 1} / {totalQuestions}</h3>
         <p className="question">{currentQuestion.question}</p>
 
         {currentQuestion.options.map((option, index) =>
           <button
-            className={"option" + (userAnswers[currentNumQuestion] === option ? " selected" : "")}
+            className={"option" + (userAnswers[currentIndexQuestion] === option ? " selected" : "")}
             onClick={() => handleSelectOption(option)}
             key={index}
           >
@@ -65,9 +59,9 @@ function Quiz() {
         )}
 
         <div className="nav-buttons">
-          <button onClick={prevQuestion} disabled={currentNumQuestion === 0}> Previous </button>
-          <button onClick={nextQuestion} disabled={!userAnswers[currentNumQuestion]}>
-            {currentNumQuestion === (questionBanks.length - 1) ? "Finish Quiz" : "Next"}
+          <button onClick={prevQuestion} disabled={currentIndexQuestion === 0}> Previous </button>
+          <button onClick={nextQuestion} disabled={!userAnswers[currentIndexQuestion]}>
+            {currentIndexQuestion === (totalQuestions - 1) ? "Finish Quiz" : "Next"}
           </button>
         </div>
       </div>
--- src/components/result.jsx
+++ src/components/result.jsx
@@ -1,12 +1,26 @@
-function Result() {
+function Result(props) {
+  function getScore() {
+    let finalScore = 0;
+
+    props.userAnswers.forEach((userAnswer, index) => {
+      if (props.questionBanks[index].answer === userAnswer) {
+        finalScore++;
+      }
+    });
+
+    return finalScore;
+  }
+
+  const userScore = getScore();
+
   return (
     <div>
       <h2>Quiz Completed!</h2>
-      <p>Your Score: 3/3</p>
+      <p>Your Score: {userScore}/{props.totalQuestions}</p>
 
-      <button className="restart-button">Restart Quiz</button>
+      <button className="restart-button" onClick={props.restartQuiz}>Restart Quiz</button>
     </div>
-  )
+  );
 }
 
 export default Result
